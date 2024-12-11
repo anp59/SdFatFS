459a349726d378e183293ca8bea04610e7d5ac18
anp59 <andreas.pachali@t-online.de>
2024-12-12 00:36:09 +0100

    enum sdcard_type_t declared in sd_defines.h

--- src/SD_SDFAT.h
+++ src/SD_SDFAT.h
@@ -3,6 +3,7 @@
 #define DISABLE_FS_H_WARNING
 #include "FS.h"
 #include "SdFat.h"
+#include "sd_defines.h"
 
 // Line with test for __has_include must not have operators or parentheses.
 #if defined __has_include
@@ -11,41 +12,30 @@
 #endif
 #endif
 
-#ifndef sdcard_type_t
-typedef enum {
-  CARD_NONE,
-  CARD_MMC,
-  CARD_SD,
-  CARD_SDHC,
-  CARD_UNKNOWN
-} sdcard_type_t;
-#endif
-
-namespace fs
-{
+namespace fs {
 class SDFATFS : public FS
 {
-public:
+  public:
     SDFATFS(FSImplPtr impl);
     bool begin(SdCsPin_t csPin = SS, uint32_t maxSck = SD_SCK_MHZ(25));
     bool begin(SdSpiConfig spiConfig);
     void end();
-    
+
     sdcard_type_t cardType();
     uint64_t cardSize() { return totalBytes(); }
     size_t numSectors();
     size_t sectorSize();
     uint64_t totalBytes();  // return (uint64_t)clusterCount() * (uint64_t)bytesPerCluster();
-    uint64_t usedBytes();   //return (uint64_t)(clusterCount() - freeClusterCount()) * (uint64_t)bytesPerCluster();
-    bool readRAW(uint8_t* buffer, uint32_t sector);
-    bool writeRAW(uint8_t* buffer, uint32_t sector);
-    SdFat& operator()();    // returns the internal SdFat-object - allow access, so users can mix SDFATFS & SdFat APIs
+    uint64_t usedBytes();   // return (uint64_t)(clusterCount() - freeClusterCount()) * (uint64_t)bytesPerCluster();
+    bool readRAW(uint8_t *buffer, uint32_t sector);
+    bool writeRAW(uint8_t *buffer, uint32_t sector);
+    SdFat &operator()();  // returns the internal SdFat-object - allow access, so users can mix SDFATFS & SdFat APIs
 };
-}   // namespace
+}  // namespace fs
 
 extern fs::SDFATFS SDF;
 
 using namespace fs;
-typedef fs::File        SDFATFile;
-typedef fs::SDFATFS     SDFATFileSystemClass;
+typedef fs::File SDFATFile;
+typedef fs::SDFATFS SDFATFileSystemClass;
 #define SDFATFSSystem SDF
